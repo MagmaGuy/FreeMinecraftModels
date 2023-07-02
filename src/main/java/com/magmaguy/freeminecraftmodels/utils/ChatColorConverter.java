5c36a932f8a15733a3280acad74f8bcab8cba965
Roughly_ <realkviiten@noreply.github.com>
2023-07-02 15:31:04 +0500

    Improved utilities

--- src/main/java/com/magmaguy/freeminecraftmodels/utils/ChatColorConverter.java
+++ src/main/java/com/magmaguy/freeminecraftmodels/utils/ChatColorConverter.java
@@ -5,21 +5,37 @@ import org.bukkit.ChatColor;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is used to convert strings to colored strings.
+ */
 public class ChatColorConverter {
 
+    /**
+     * Private constructor to prevent instantiation.
+     */
     private ChatColorConverter() {
+        throw new IllegalStateException("Utility class");
     }
 
+    /**
+     * Converts a string to a colored string.
+     * @param string The string to convert.
+     * @return The colored string.
+     */
     public static String convert(String string) {
         if (string == null) return "";
         return ChatColor.translateAlternateColorCodes('&', string);
     }
 
+    /**
+     * Converts a list of strings to a list of colored strings.
+     * @param list The list of strings to convert.
+     * @return The list of colored strings.
+     * @see ChatColorConverter#convert(String)
+     */
     public static List<String> convert(List<String> list) {
-        List<String> convertedList = new ArrayList<>();
-        for (String string : list)
-            convertedList.add(convert(string));
-        return convertedList;
+        list.replaceAll(ChatColorConverter::convert);
+        return list;
     }
 
 }
--- src/main/java/com/magmaguy/freeminecraftmodels/utils/Developer.java
+++ src/main/java/com/magmaguy/freeminecraftmodels/utils/Developer.java
@@ -2,14 +2,45 @@ package com.magmaguy.freeminecraftmodels.utils;
 
 import org.bukkit.Bukkit;
 
+import java.util.logging.Logger;
+
+/**
+ * This class is used to log messages to the console.
+ */
 public class Developer {
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private Developer() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * The logger used to log messages to the console.
+     */
+    private static final Logger LOGGER = Bukkit.getLogger();
+
+    /**
+     * Logs an info message to the console.
+     * @param message The message to log.
+     */
     public static void info(String message) {
-        Bukkit.getLogger().info("[FreeMinecraftModels] " + message);
+        LOGGER.info("[FreeMinecraftModels] " + message);
     }
+
+    /**
+     * Logs a warning message to the console.
+     * @param message The message to log.
+     */
     public static void warn(String message) {
-        Bukkit.getLogger().warning("[FreeMinecraftModels] " + message);
+        LOGGER.warning("[FreeMinecraftModels] " + message);
     }
+
+    /**
+     * Logs a debug message to the console.
+     * @param message The message to log.
+     */
     public static void debug(String message) {
-        Bukkit.getLogger().warning("[FreeMinecraftModels] DEBUG: " + message);
+        LOGGER.warning("[FreeMinecraftModels] DEBUG: " + message);
     }
 }
