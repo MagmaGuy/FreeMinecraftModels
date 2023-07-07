f869e79d5de1f0168bf786f9394fb0e2310bd40a
BuildTools <unconfigured@null.spigotmc.org>
2023-07-07 05:16:16 +0100

    - Implemented nametags

--- src/main/java/com/magmaguy/freeminecraftmodels/customentity/StaticEntity.java
+++ src/main/java/com/magmaguy/freeminecraftmodels/customentity/StaticEntity.java
@@ -3,11 +3,11 @@ package com.magmaguy.freeminecraftmodels.customentity;
 import com.magmaguy.freeminecraftmodels.dataconverter.Bone;
 import com.magmaguy.freeminecraftmodels.dataconverter.FileModelConverter;
 import com.magmaguy.freeminecraftmodels.dataconverter.Skeleton;
-import com.magmaguy.freeminecraftmodels.entities.ModelArmorStand;
 import com.magmaguy.freeminecraftmodels.utils.Developer;
 import lombok.Getter;
 import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.Vector;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -23,6 +23,8 @@ public class StaticEntity {
     @Getter
     private final String entityID;
     private final ArrayList<ArmorStand> armorStandList = new ArrayList<>();
+    @Getter
+    private final String name = "default";
 
     private StaticEntity(String entityID, Location targetLocation) {
         this.entityID = entityID;
@@ -47,10 +49,15 @@ public class StaticEntity {
         return new StaticEntity(entityID, targetLocation);
     }
 
+    private static boolean isNameTag(ArmorStand armorStand) {
+        return armorStand.getPersistentDataContainer().has(Bone.nameTagKey, PersistentDataType.BYTE);
+    }
+
     private void armorStandInitializer(Location targetLocation, Bone bone) {
-        if (!bone.getCubeChildren().isEmpty()) {
+        if (!bone.getCubeChildren().isEmpty() || bone.isNameTag()) {
             ArmorStand armorStand = bone.generateDisplay(targetLocation);
             armorStandList.add(armorStand);
+            if (bone.isNameTag()) armorStand.setMarker(false);
         }
         if (!bone.getBoneChildren().isEmpty())
             bone.getBoneChildren().forEach(boneChild -> armorStandInitializer(targetLocation, boneChild));
@@ -58,5 +65,45 @@ public class StaticEntity {
 
     public void remove() {
         for (ArmorStand armorStand : armorStandList) armorStand.remove();
+        armorStandList.clear();
+    }
+
+    /**
+     * Sets the custom name that is visible in-game on the entity
+     *
+     * @param name Name to set
+     */
+    public void setName(String name) {
+        armorStandList.forEach(armorStand -> {
+            if (isNameTag(armorStand))
+                armorStand.setCustomName(name);
+
+        });
+    }
+
+    /**
+     * Default is false
+     *
+     * @param display Sets whether the name is visible
+     */
+    public void setNameVisible(boolean display) {
+        armorStandList.forEach(armorStand -> {
+            if (isNameTag(armorStand))
+                armorStand.setCustomNameVisible(display);
+        });
+    }
+
+    /**
+     * Returns the name tag locations. Useful if you want to add more text above or below them.
+     * Not currently guaranteed to be the exact location.
+     *
+     * @return
+     */
+    public List<Location> getNameTagLocations() {
+        List<Location> locations = new ArrayList<>();
+        armorStandList.forEach(armorStand -> {
+            if (isNameTag(armorStand)) locations.add(armorStand.getLocation().add(new Vector(0, 1.9875, 0)));
+        });
+        return locations;
     }
 }
--- src/main/java/com/magmaguy/freeminecraftmodels/entities/ModelArmorStand.java
+++ src/main/java/com/magmaguy/freeminecraftmodels/entities/ModelArmorStand.java
@@ -1,5 +1,6 @@
 package com.magmaguy.freeminecraftmodels.entities;
 
+import com.magmaguy.freeminecraftmodels.dataconverter.Bone;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,16 +14,19 @@ public class ModelArmorStand {
     private ModelArmorStand() {
     }
 
-    public static ArmorStand generate(Location location, int modelID) {
-       location.setDirection(new Vector(0,0,-1));
-        return (ArmorStand) location.getWorld().spawn(location, EntityType.ARMOR_STAND.getEntityClass(), entity -> applyFeatures((ArmorStand) entity, modelID));
+    public static ArmorStand generate(Location location, int modelID, Bone bone) {
+        location.setDirection(new Vector(0, 0, -1));
+        return (ArmorStand) location.getWorld().spawn(location, EntityType.ARMOR_STAND.getEntityClass(),
+                entity -> applyFeatures((ArmorStand) entity, modelID, bone));
     }
 
-    private static void applyFeatures(ArmorStand armorStand, int modelID) {
+    private static void applyFeatures(ArmorStand armorStand, int modelID, Bone bone) {
         armorStand.setGravity(false);
         armorStand.setMarker(true);
         armorStand.setPersistent(false);
         armorStand.setVisible(false);
+        //This should only really be true for name tags and maybe other utility bones later on
+        if (bone.getCubeChildren().isEmpty()) return;
         ItemStack leatherHorseArmor = new ItemStack(Material.LEATHER_HORSE_ARMOR);
         LeatherArmorMeta itemMeta = (LeatherArmorMeta) leatherHorseArmor.getItemMeta();
         itemMeta.setColor(Color.WHITE);
