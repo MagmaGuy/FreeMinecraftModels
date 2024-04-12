3072c4a1b8992ccbb8547caa9069298d5be4e5a2
BuildTools <unconfigured@null.spigotmc.org>
2024-04-12 05:36:47 +0100

    - Now fully working for 1.20+ display entities

---
+++ src/main/java/com/magmaguy/freeminecraftmodels/customentity/core/BoneTransforms.java
@@ -0,0 +1,195 @@
+package com.magmaguy.freeminecraftmodels.customentity.core;
+
+import com.magmaguy.easyminecraftgoals.NMSManager;
+import com.magmaguy.easyminecraftgoals.internal.PacketModelEntity;
+import com.magmaguy.freeminecraftmodels.config.DefaultConfig;
+import com.magmaguy.freeminecraftmodels.dataconverter.BoneBlueprint;
+import com.magmaguy.freeminecraftmodels.utils.Developer;
+import com.magmaguy.freeminecraftmodels.utils.TransformationMatrix;
+import com.magmaguy.freeminecraftmodels.utils.VersionChecker;
+import lombok.Getter;
+import org.bukkit.Location;
+import org.bukkit.util.EulerAngle;
+import org.bukkit.util.Vector;
+
+public class BoneTransforms {
+
+    private final TransformationMatrix specialMatrix = new TransformationMatrix();
+    private final Bone parent;
+    private final Bone bone;
+    private final TransformationMatrix localMatrix = new TransformationMatrix();
+    TransformationMatrix globalTestRotationMatrix = new TransformationMatrix();
+    Vector testFinalLocation = new Vector();
+    Vector testFinalRotation = new Vector();
+    private TransformationMatrix globalMatrix = new TransformationMatrix();
+    @Getter
+    private PacketModelEntity packetArmorStandEntity = null;
+    @Getter
+    private PacketModelEntity packetDisplayEntity = null;
+
+    public BoneTransforms(Bone bone, Bone parent) {
+        this.bone = bone;
+        this.parent = parent;
+    }
+
+    public void transform() {
+        updateLocalTransform();
+        updateGlobalTransform();
+    }
+
+    public void updateGlobalTransform() {
+        if (parent != null) {
+//            TransformationMatrix.multiplyMatrices(localMatrix, parent.getBoneTransforms().globalMatrix, globalMatrix);
+            TransformationMatrix.multiplyMatrices(parent.getBoneTransforms().globalMatrix, localMatrix, globalMatrix);
+        } else {
+            globalMatrix = localMatrix;
+        }
+    }
+
+    public void updateLocalTransform() {
+        localMatrix.resetToIdentityMatrix();
+        //Shift to model center
+        translateModelCenter();
+
+        //Add the pivot point for the rotation - is removed later
+        shiftPivotPoint();
+
+        rotateDefaultBoneRotation();
+
+        translateAnimation();
+
+        rotateAnimation();
+
+        shiftPivotPointBack();
+
+        rotateByEntityYaw();
+    }
+
+    //Shift to model center
+    private void translateModelCenter() {
+        localMatrix.translate(bone.getBoneBlueprint().getModelCenter());
+
+        //The bone is relative to its parent, so remove the offset of the parent
+        if (parent != null) localMatrix.translate(parent.getBoneBlueprint().getModelCenter().multiply(-1));
+    }
+
+    private void shiftPivotPoint() {
+        localMatrix.translate(bone.getBoneBlueprint().getBlueprintModelPivot().multiply(-1));
+    }
+
+    private void translateAnimation() {
+        localMatrix.translate(
+                (float) bone.getAnimationTranslation().getX(),
+                (float) bone.getAnimationTranslation().getY(),
+                (float) bone.getAnimationTranslation().getZ());
+    }
+
+    private void rotateAnimation() {
+        Vector test = new Vector(bone.getAnimationRotation().getX(), bone.getAnimationRotation().getY(), -bone.getAnimationRotation().getZ());
+        test.rotateAroundY(Math.PI);
+//        localMatrix.rotate(
+//                (float) -bone.getAnimationRotation().getX(),
+//                (float) bone.getAnimationRotation().getY(),
+//                (float) -bone.getAnimationRotation().getZ());
+        localMatrix.rotate(
+                (float) test.getX(),
+                (float) test.getY(),
+                (float) test.getZ());
+    }
+
+    private void rotateDefaultBoneRotation() {
+        localMatrix.rotate(
+                (float) bone.getBoneBlueprint().getBlueprintOriginalBoneRotation().getX(),
+                (float) bone.getBoneBlueprint().getBlueprintOriginalBoneRotation().getY(),
+                (float) bone.getBoneBlueprint().getBlueprintOriginalBoneRotation().getZ());
+    }
+
+    private void shiftPivotPointBack() {
+        //Remove the pivot point, go back to the model center
+        localMatrix.translate(bone.getBoneBlueprint().getBlueprintModelPivot());
+    }
+
+    public void generateDisplay() {
+        transform();
+        specialMatrix.rotate(0, (float) Math.PI, 0);
+        if (bone.getBoneBlueprint().getBoneName().contains("bone10"))
+            Developer.warn(String.format("rotation post generation %s, %s, %s", Math.toDegrees(globalMatrix.getRotation()[0]), Math.toDegrees(globalMatrix.getRotation()[1]), Math.toDegrees(globalMatrix.getRotation()[2])));
+        if (bone.getBoneBlueprint().isDisplayModel()) {
+            initializeDisplayEntityBone();
+            initializeArmorStandBone();
+        }
+    }
+
+    private void initializeDisplayEntityBone() {
+        if (!DefaultConfig.useDisplayEntitiesWhenPossible) return;
+        packetDisplayEntity = NMSManager.getAdapter().createPacketDisplayEntity(getDisplayEntityTargetLocation());
+        packetDisplayEntity.initializeModel(getDisplayEntityTargetLocation(), bone.getBoneBlueprint().getModelID());
+        packetDisplayEntity.setScale(2.5f);
+        packetDisplayEntity.sendLocationAndRotationPacket(getDisplayEntityTargetLocation(), getDisplayEntityRotation());
+    }
+
+    private void initializeArmorStandBone() {
+        //todo: add way to disable armor stands later via config
+        packetArmorStandEntity = NMSManager.getAdapter().createPacketArmorStandEntity(getArmorStandTargetLocation());
+        packetArmorStandEntity.initializeModel(getArmorStandTargetLocation(), bone.getBoneBlueprint().getModelID());
+        packetArmorStandEntity.sendLocationAndRotationPacket(getArmorStandTargetLocation(), getArmorStandEntityRotation());
+    }
+
+    private void rotateByEntityYaw() {
+        //rotate by yaw amount
+        if (parent == null) {
+            localMatrix.rotate(0, (float) -Math.toRadians(bone.getSkeleton().getCurrentLocation().getYaw() + 180), 0);
+        }
+    }
+
+    protected Location getArmorStandTargetLocation() {
+        float[] translatedGlobalMatrix = globalMatrix.applyTransformation(new float[]{0, 0, 0, 1});
+        Location armorStandLocation = new Location(bone.getSkeleton().getCurrentLocation().getWorld(),
+                translatedGlobalMatrix[0],
+                translatedGlobalMatrix[1],
+                translatedGlobalMatrix[2])
+                .add(bone.getSkeleton().getCurrentLocation());
+        armorStandLocation.setYaw(180);
+        armorStandLocation.subtract(new Vector(0, BoneBlueprint.getARMOR_STAND_PIVOT_POINT_HEIGHT(), 0));
+        return armorStandLocation;
+    }
+
+    protected Location getDisplayEntityTargetLocation() {
+        float[] translatedGlobalMatrix = globalMatrix.applyTransformation(new float[]{0, 0, 0, 1});
+        Location armorStandLocation;
+        if (!VersionChecker.serverVersionOlderThan(20, 0)) {
+//            testFinalLocation = new Vector(-translatedGlobalMatrix[0], translatedGlobalMatrix[1], -translatedGlobalMatrix[2]);
+//            testFinalLocation.rotateAroundY(Math.PI);
+            armorStandLocation = new Location(bone.getSkeleton().getCurrentLocation().getWorld(),
+                    translatedGlobalMatrix[0],
+                    translatedGlobalMatrix[1],
+                    translatedGlobalMatrix[2])
+                    .add(bone.getSkeleton().getCurrentLocation());
+            armorStandLocation.setYaw(180);
+        } else
+            armorStandLocation = new Location(bone.getSkeleton().getCurrentLocation().getWorld(),
+                    translatedGlobalMatrix[0],
+                    translatedGlobalMatrix[1],
+                    translatedGlobalMatrix[2])
+                    .add(bone.getSkeleton().getCurrentLocation());
+        return armorStandLocation;
+    }
+
+    protected EulerAngle getDisplayEntityRotation() {
+        float[] rotation = globalMatrix.getRotation();
+        if (VersionChecker.serverVersionOlderThan(20, 0))
+            return new EulerAngle(rotation[0], rotation[1], rotation[2]);
+        else {
+//            testFinalRotation = new Vector(-rotation[0], rotation[1], -rotation[2]);
+//            testFinalRotation.rotateAroundY(Math.PI);
+            return new EulerAngle(-rotation[0], rotation[1], -rotation[2]);
+//            return new EulerAngle( testFinalRotation.getX(), testFinalRotation.getY(), testFinalRotation.getZ());
+        }
+    }
+
+    protected EulerAngle getArmorStandEntityRotation() {
+        float[] rotation = globalMatrix.getRotation();
+        return new EulerAngle(rotation[0], rotation[1], rotation[2]);
+    }
+
+}
--- src/main/java/com/magmaguy/freeminecraftmodels/utils/TransformationMatrix.java
+++ src/main/java/com/magmaguy/freeminecraftmodels/utils/TransformationMatrix.java
@@ -146,4 +146,129 @@ public class TransformationMatrix {
         return rotation; // Returns rotations in radians
     }
 
+
+//        /**
+//     * Extracts a rotation in radians using the ZYX convention
+//     *
+//     * @return [x, y, z]
+//     */
+//    public float[] getRotation() {
+//        // Assuming the rotation matrix is "pure" (no scaling) and follows ZYX order
+//        float[] rotation = new float[3];
+//        // Yaw (rotation around Z axis)
+//        rotation[2] = (float) Math.atan2(-matrix[0][1], matrix[0][0]);
+//        // Check the special case where cos(yaw) is close to 0
+//        if (Math.abs(matrix[0][1]) < 1e-6 && Math.abs(matrix[0][0]) < 1e-6) {
+//            // Pitch (rotation around Y axis)
+//            rotation[1] = (float) Math.atan2(matrix[0][2], matrix[1][2]);
+//            // Roll (rotation around X axis) is indeterminate: set to 0 or use previous value
+//            rotation[0] = 0;
+//        } else {
+//            // Pitch (rotation around Y axis)
+//            rotation[1] = (float) Math.atan2(-matrix[1][2], matrix[2][2]);
+//            // Roll (rotation around X axis)
+//            rotation[0] = (float) Math.atan2(-matrix[2][1], matrix[1][1]);
+//        }
+//        return rotation; // Returns rotations in radians
+//    }
+
+//        /**
+//     * Extracts a rotation in radians using the ZXY convention
+//     *
+//     * @return [x, y, z]
+//     */
+//    public float[] getRotation() {
+//        // Assuming the rotation matrix is "pure" (no scaling) and follows ZXY order
+//        float[] rotation = new float[3];
+//        // Yaw (rotation around Z axis)
+//        rotation[2] = (float) Math.atan2(matrix[1][0], matrix[0][0]);
+//
+//        // Check the special case where cos(yaw) is close to 0
+//        if (Math.abs(matrix[1][0]) < 1e-6 && Math.abs(matrix[0][0]) < 1e-6) {
+//            // Pitch (rotation around Y axis)
+//            rotation[1] = (float) Math.atan2(-matrix[2][0], matrix[1][1]);
+//            // Roll (rotation around X axis) is indeterminate: set to 0 or use previous value
+//            rotation[0] = 0;
+//        } else {
+//            // Roll (rotation around X axis)
+//            rotation[0] = (float) Math.atan2(matrix[2][1], matrix[2][2]);
+//            // Pitch (rotation around Y axis)
+//            rotation[1] = (float) Math.atan2(matrix[1][2], matrix[1][1]);
+//        }
+//        return rotation; // Returns rotations in radians
+//    }
+
+//        /**
+//     * Extracts a rotation in radians using the XZY convention
+//     *
+//     * @return [x, y, z]
+//     */
+//    public float[] getRotation() {
+//        // Assuming the rotation matrix is "pure" (no scaling) and follows XZY order
+//        float[] rotation = new float[3];
+//        // Roll (rotation around X axis)
+//        rotation[0] = (float) Math.atan2(-matrix[1][2], matrix[2][2]);
+//        // As a special case, if cos(roll) is close to 0, use an alternative calculation
+//        if (Math.abs(matrix[1][2]) < 1e-6 && Math.abs(matrix[2][2]) < 1e-6) {
+//            // Yaw (rotation around Z axis)
+//            rotation[2] = (float) Math.atan2(-matrix[0][1], matrix[0][0]);
+//            // Pitch (rotation around Y axis) is indeterminate: set to 0 or use previous value
+//            rotation[1] = 0;
+//        } else {
+//            // Yaw (rotation around Z axis)
+//            rotation[2] = (float) Math.atan2(matrix[0][1], matrix[1][1]);
+//            // Pitch (rotation around Y axis)
+//            rotation[1] = (float) Math.atan2(-matrix[2][0], matrix[0][0]);
+//        }
+//        return rotation; // Returns rotations in radians
+//    }
+
+    public void changeBasis(Vector newBasisX, Vector newBasisY, Vector newBasisZ) {
+        TransformationMatrix basisChangeMatrix = new TransformationMatrix();
+        // Set the matrix columns to the new basis vectors
+        basisChangeMatrix.matrix[0][0] = (float) newBasisX.getX();
+        basisChangeMatrix.matrix[1][0] = (float) newBasisX.getY();
+        basisChangeMatrix.matrix[2][0] = (float) newBasisX.getZ();
+
+        basisChangeMatrix.matrix[0][1] = (float) newBasisY.getX();
+        basisChangeMatrix.matrix[1][1] = (float) newBasisY.getY();
+        basisChangeMatrix.matrix[2][1] = (float) newBasisY.getZ();
+
+        basisChangeMatrix.matrix[0][2] = (float) newBasisZ.getX();
+        basisChangeMatrix.matrix[1][2] = (float) newBasisZ.getY();
+        basisChangeMatrix.matrix[2][2] = (float) newBasisZ.getZ();
+
+        // Since this matrix transforms from the new basis to the original, to change the basis of this matrix,
+        // we multiply it with the basisChangeMatrix.
+        multiplyWith(basisChangeMatrix);
+    }
+
+    public TransformationMatrix changeTo180DegYBasis() {
+        Vector newX = new Vector(-1, 0, 0); // New X-axis after 180 rotation around Y
+        Vector newY = new Vector(0, 1, 0);  // Y-axis remains unchanged
+        Vector newZ = new Vector(0, 0, -1); // New Z-axis after 180 rotation around Y
+
+        changeBasis(newX, newY, newZ);
+
+        return this;
+    }
+
+    public void flipX() {
+        TransformationMatrix flipMatrix = new TransformationMatrix();
+        flipMatrix.matrix[0][0] = -1;
+        multiplyWith(flipMatrix);
+    }
+
+    public void flipY() {
+        TransformationMatrix flipMatrix = new TransformationMatrix();
+        flipMatrix.matrix[1][1] = -1;
+        multiplyWith(flipMatrix);
+    }
+
+    public void flipZ() {
+        TransformationMatrix flipMatrix = new TransformationMatrix();
+        flipMatrix.matrix[2][2] = -1;
+        multiplyWith(flipMatrix);
+    }
+
 }
