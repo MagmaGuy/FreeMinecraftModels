41a3fbcfe4d9106a73d19359355ef49ecbd0e20e
MagmaGuy <tiagoarnaut@gmail.com>
2026-06-18 08:14:06 +0100

    Rename EliteMobs Lua loot helpers

--- src/main/java/com/magmaguy/freeminecraftmodels/scripting/LuaWorldEnricher.java
+++ src/main/java/com/magmaguy/freeminecraftmodels/scripting/LuaWorldEnricher.java
@@ -30,13 +30,36 @@ public final class LuaWorldEnricher {
     }
 
     private static void enrich(LuaTable table, World world) {
-        table.set("drop_elitemobs_loot", LuaTableSupport.tableMethod(table, args ->
-                dropEliteMobsLoot(world, args)));
-        table.set("drop_elitemobs_item", LuaTableSupport.tableMethod(table, args ->
-                dropEliteMobsItem(world, args)));
+        table.set("drop_elitemobs_procedural_loot", LuaTableSupport.tableMethod(table, args ->
+                dropEliteMobsProceduralLoot(world, args)));
+        table.set("drop_elitemobs_random_loot", LuaTableSupport.tableMethod(table, args ->
+                dropEliteMobsRandomLoot(world, args)));
     }
 
-    private static LuaValue dropEliteMobsLoot(World defaultWorld, Varargs args) {
+    private static LuaValue dropEliteMobsProceduralLoot(World defaultWorld, Varargs args) {
+        if (!Bukkit.getPluginManager().isPluginEnabled("EliteMobs")) {
+            return LuaValue.FALSE;
+        }
+
+        Player player = resolvePlayer(args.arg(1));
+        if (player == null) {
+            return LuaValue.FALSE;
+        }
+
+        int level = args.checkint(2);
+        Location location = resolveLocation(args.arg(3), defaultWorld);
+        if (location == null || location.getWorld() == null) {
+            location = player.getLocation();
+        }
+
+        try {
+            return LuaValue.valueOf(EliteMobsLootDropper.dropProceduralLoot(player, level, location));
+        } catch (NoClassDefFoundError ignored) {
+            return LuaValue.FALSE;
+        }
+    }
+
+    private static LuaValue dropEliteMobsRandomLoot(World defaultWorld, Varargs args) {
         if (!Bukkit.getPluginManager().isPluginEnabled("EliteMobs")) {
             return LuaValue.FALSE;
         }
@@ -54,30 +77,6 @@ public final class LuaWorldEnricher {
 
         try {
             return LuaValue.valueOf(EliteMobsLootDropper.dropRandomLoot(player, level, location));
-        } catch (NoClassDefFoundError ignored) {
-            return LuaValue.FALSE;
-        }
-    }
-
-    private static LuaValue dropEliteMobsItem(World defaultWorld, Varargs args) {
-        if (!Bukkit.getPluginManager().isPluginEnabled("EliteMobs")) {
-            return LuaValue.FALSE;
-        }
-
-        Player player = resolvePlayer(args.arg(1));
-        if (player == null) {
-            return LuaValue.FALSE;
-        }
-
-        String filename = args.checkjstring(2);
-        int level = args.checkint(3);
-        Location location = resolveLocation(args.arg(4), defaultWorld);
-        if (location == null || location.getWorld() == null) {
-            location = player.getLocation();
-        }
-
-        try {
-            return LuaValue.valueOf(EliteMobsLootDropper.dropCustomItem(player, filename, level, location));
         } catch (NoClassDefFoundError ignored) {
             return LuaValue.FALSE;
         }
